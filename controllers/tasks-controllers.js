3a4ebff1acf8d27d63a496aa1d9486f365f0b687
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-03-20 09:36:00 +1300

    error handling finished, added PORT variable

--- controllers/tasks-controllers.js
+++ controllers/tasks-controllers.js
@@ -1,5 +1,6 @@
 const Task = require('../models/task-models')
 const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
 // Controllers
 const getAllTasks = asyncWrapper(async (req, res) => {
@@ -16,9 +17,7 @@ const getTask = asyncWrapper(async (req, res, next)=> {
     const { id: taskID } = req.params
     const task = await Task.findOne({ _id: taskID })
     if(!task) {
-        const error = new Error('Not Found');
-        error.status = 404;
-        return next(error)
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
     res.status(200).json({ task })
 })
@@ -30,7 +29,7 @@ const updateTask = asyncWrapper(async (req, res)=> {
         runValidators: true,
     })
     if(!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}`})
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
     res.status(200).json({ task })
 })
@@ -39,7 +38,7 @@ const deleteTask = asyncWrapper(async (req, res)=> {
     const { id: taskID } = req.params
     const task = await Task.findOneAndDelete({ _id: taskID })
     if(!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}`})
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
     res.status(200).json({ task })
 })
